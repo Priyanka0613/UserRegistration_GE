c30fa80362acca63b103bddd9473c36161886e28
Priyanka <priyankaag.0613@gmail.com>
2023-12-28 17:14:58 +0530

    UC1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("User registration");
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter first name:");
+        String name=s.nextLine();
+
+        String namePattern= "^[A-Z]{1}[A-Za-z]{3,}";
+        Pattern pattern = Pattern.compile(namePattern);
+        Matcher matcher=pattern.matcher(name);
+        System.out.println(matcher.matches());
+        s.close();
     }
 }
