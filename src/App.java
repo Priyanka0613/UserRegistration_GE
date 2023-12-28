09707ac8143817c80c1c2ba5efdc7bb00b9ca091
Priyanka <priyankaag.0613@gmail.com>
2023-12-28 19:22:42 +0530

    UC3

--- src/App.java
+++ src/App.java
@@ -9,18 +9,26 @@ public class App {
         String firstname=s.nextLine();
         System.out.println("Enter last name:");
         String lastname=s.nextLine();
+        System.out.println("Enter email:");
+        String emailname=s.nextLine();
 
-        //firstname
-        String firstnamePattern= "^[A-Z]{1}[A-Za-z]{3,}";
+        // //firstname
+        String firstnamePattern= "^[A-Z]{1}[A-Za-z]{3,}$";
         Pattern firstpattern = Pattern.compile(firstnamePattern);
         Matcher firstnamematcher=firstpattern.matcher(firstname);
         System.out.println(firstnamematcher.matches());
 
-        //lastname
-        String lastnamePattern= "^[A-Z]{1}[A-Za-z]{3,}";
+        // //lastname
+        String lastnamePattern= "^[A-Z]{1}[A-Za-z]{3,}$";
         Pattern lastpattern = Pattern.compile(lastnamePattern);
         Matcher lastnamematcher=lastpattern.matcher(lastname);
         System.out.println(lastnamematcher.matches());
+
+        //email
+        String emailPattern= "^[a-zA-Z0-9]+(\\.|\\_|\\-|\\+)?[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.+[a-zA-Z]{1,}.[a-zA-Z]*$"; //abc.@gmail.com true
+        Pattern emailpattern = Pattern.compile(emailPattern);
+        Matcher emailmatcher=emailpattern.matcher(emailname);
+        System.out.println(emailmatcher.matches());
 
         s.close();
     }
